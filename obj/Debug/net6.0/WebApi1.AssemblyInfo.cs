830d38f7110606636a6ed795bd3f0fa76097e666
AmmarHabbab <ammarhabbab118@gmail.com>
2024-11-17 22:10:24 +0300

    successfully created a token

--- obj/Debug/net6.0/WebApi1.AssemblyInfo.cs
+++ obj/Debug/net6.0/WebApi1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf76d4f177efd2f63ec1e879a379c69907da9e64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ad48149dfbe5ca86a0c14b3d4e506474efe3031")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
